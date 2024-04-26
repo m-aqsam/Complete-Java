362eaa8319a73f9c32cb4523e6d5ef1f78a26ed7
m_aqsam <maqsam1155@gmail.com>
2024-04-27 02:46:09 +0500

    first program

---
+++ Firstprogram.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import Inheritance.Methodes;
+
+public class Firstprogram {
+
+    public static void main(String[] args) {
+        int num =8;
+        int i =1;
+        int result=num +""
+
+    }
+
+}
